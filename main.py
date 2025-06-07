b1963732b170f7c0a6442ab156901d57ec3b83dd
NE-Ninny <neninny@proton.me>
2025-06-07 18:16:27 -0400

    started building the monthly_budget function

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+def monthly_budget(annual_budget):
+    print(" ")
+    print("============================== INSTABUDGET ==============================")
+    print(f"Here's your budget based on your ${annual_budget} a year budget!: ")
+
+
+
+
 introduction = input("Please provide a name: ") 
 annual_salary = input(f"Hello, {introduction}! What is your annual salary?: ")
 
-print(f"This is what your budget should be based on your annual salary of ${annual_salary}")
+monthly_budget(annual_salary)
