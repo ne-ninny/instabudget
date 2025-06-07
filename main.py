2f47e7568112c2f2894fbd1b34e83f54c4b11061
NE-Ninny <neninny@proton.me>
2025-06-07 17:46:07 -0400

    Added .gitignore and built the start of the welcome message

---
+++ main.py
@@ -0,0 +1,4 @@
+introduction = input("Please provide a name: ") 
+annual_salary = input(f"Hello, {introduction}! What is your annual salary?: ")
+
+print(f"This is what your budget should be based on your annual salary of ${annual_salary}")
