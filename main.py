6e97e3214fba2424283406e4b1bcfaf0eb81b783
NE-Ninny <neninny@proton.me>
2025-06-07 18:24:06 -0400

    added basic layout of printed budget

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ def monthly_budget(annual_budget):
     print("============================== INSTABUDGET ==============================")
     print(f"Here's your budget based on your ${annual_budget} a year budget!: ")
 
+    print("""
+          SAVINGS (40%): 
+          HOUSING (20%):
+          TRANSPORTATION (10%):
+          FOOD (10%):
+          HEALTH/MEDICAL (5%):
+          UTILITIES (3%):
+          WHATEVER (10%):
+          
+          TOTAL (98%):
+          LEFT OVER (2%):
+          """)
+
 
 
 
