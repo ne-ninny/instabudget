00507ed00751cf2f85b729f41ede70b64af06310
Ninny <neninny@proton.me>
2025-06-07 23:58:54 -0400

    added allocation of money to areas of budget

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 def monthly_budget(annual_budget):
+    one_month = annual_budget / 12
+    
     print(" ")
     print("============================== INSTABUDGET ==============================")
     print(f"Here's your budget based on your ${annual_budget} a year budget!: ")
-
-    print("""
-           SAVINGS (40%): 
-           HOUSING (20%):
-    TRANSPORTATION (10%):
-              FOOD (10%):
-     HEALTH/MEDICAL (5%):
-          UTILITIES (3%):
-          WHATEVER (10%):
+    
+    print(f"""
+           SAVINGS (40%): ${one_month * 0.4}
+           HOUSING (20%): ${one_month * 0.2}
+    TRANSPORTATION (10%): ${one_month * 0.1}
+              FOOD (10%): ${one_month * 0.1}
+     HEALTH/MEDICAL (5%): ${one_month * 0.05}
+          UTILITIES (3%): ${one_month * 0.03}
+          WHATEVER (10%): ${one_month * 0.1}
           
-             TOTAL (98%):
-          LEFT OVER (2%):
+             TOTAL (98%): ${one_month * 0.98}
+          LEFT OVER (2%): ${one_month * 0.02}
           """)
-
-
-
+# 
+# 
+# 
 
 introduction = input("Please provide a name: ") 
 annual_salary = input(f"Hello, {introduction}! What is your annual salary?: ")
 
-monthly_budget(annual_salary)
+monthly_budget(100000)
